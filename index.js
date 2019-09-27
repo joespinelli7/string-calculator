6ad9d81e63559d0e9c8716ec9f86459cbee2c824
github username <josephspinelli@yahoo.com>
2019-09-26 20:05:48 -0500

    step 3 complete

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function stringCalculator(str) {
-  // splits str wherever the comma delimiter is into an array of unlimited length.
-  const strArr = str.split(',');
+  // splits str using regex expression, wherever the comma or newline character delimiter is into an array.
+  const strArr = str.split(/,|\n/) ;
 
   // iterate over the strArr and check if each number is actually a number by using parseInt which will return
   // NaN if we're dealing with anything over than numbers. If element is something other than a number return 0.
--- test.js
+++ test.js
@@ -1,16 +1,21 @@
 const stringCalculator = require('./index');
 
 test('stringCalculator function exists', () => {
-expect(typeof stringCalculator).toEqual('function');
+  expect(typeof stringCalculator).toEqual('function');
 });
 
 test('Support a maximum of 2 numbers using a comma delimiter', () => {
-expect(stringCalculator('20')).toEqual(20);
-expect(stringCalculator('1,5000')).toEqual(5001);
-expect(stringCalculator('5,tytyt')).toEqual(5);
+  expect(stringCalculator('20')).toEqual(20);
+  expect(stringCalculator('1,5000')).toEqual(5001);
+  expect(stringCalculator('5,tytyt')).toEqual(5);
 })
 
 test('Support an unlimited number of numbers', () => {
-expect(stringCalculator('1,2,3,4,5,6,7,8,9,10,11,12')).toEqual(78);
-expect(stringCalculator('1,a,2,b,3,c')).toEqual(6);
+  expect(stringCalculator('1,2,3,4,5,6,7,8,9,10,11,12')).toEqual(78);
+  expect(stringCalculator('1,a,2,b,3,c')).toEqual(6);
+})
+
+test('Support a newline character as an alternative delimiter', () => {
+  expect(stringCalculator('1\n2,3')).toEqual(6);
+  expect(stringCalculator('5\nabc\n5\nxyz\n5')).toEqual(15);
 })
