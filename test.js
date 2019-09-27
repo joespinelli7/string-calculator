9d6f523f46ebfb03db6b2ddee34e3ba31c17c9c9
github username <josephspinelli@yahoo.com>
2019-09-26 19:54:05 -0500

    step 2 complete

--- test.js
+++ test.js
@@ -9,3 +9,8 @@ expect(stringCalculator('20')).toEqual(20);
 expect(stringCalculator('1,5000')).toEqual(5001);
 expect(stringCalculator('5,tytyt')).toEqual(5);
 })
+
+test('Support an unlimited number of numbers', () => {
+expect(stringCalculator('1,2,3,4,5,6,7,8,9,10,11,12')).toEqual(78);
+expect(stringCalculator('1,a,2,b,3,c')).toEqual(6);
+})
