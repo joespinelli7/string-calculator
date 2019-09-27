0e86bf4f446bdad39a0e4ef93c663c788691de3e
github username <josephspinelli@yahoo.com>
2019-09-26 19:52:26 -0500

    step 1 complete

---
+++ index.js
@@ -0,0 +1,20 @@
+function stringCalculator(str) {
+  // splits str wherever the comma delimiter is into an array of max length 2.
+  const strArr = str.split(',', 2);
+
+  // iterate over the strArr and check if each number is actually a number by using parseInt which will return
+  // NaN if we're dealing with anything over than numbers. If element is something other than a number return 0.
+  const sumArr = strArr.map(num => {
+    if (parseInt(num)) {
+      return parseInt(num);
+    } else {
+      return 0;
+    }
+  })
+
+  // finally, get sum of all elements inside the array using reduce and return the final sum.
+  const sum = sumArr.reduce((total, num) => total + num);
+  return sum;
+}
+
+module.exports = stringCalculator;
--- test.js
+++ test.js
@@ -1,1 +1,11 @@
+const stringCalculator = require('./index');
 
+test('stringCalculator function exists', () => {
+expect(typeof stringCalculator).toEqual('function');
+});
+
+test('Support a maximum of 2 numbers using a comma delimiter', () => {
+expect(stringCalculator('20')).toEqual(20);
+expect(stringCalculator('1,5000')).toEqual(5001);
+expect(stringCalculator('5,tytyt')).toEqual(5);
+})
